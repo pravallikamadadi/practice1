79364790edc921cac581631bc2a34aa0368d490e
pravallika madadi <madadi.pravalika@gmail.com>
2017-03-20 17:56:18 -0500

    First commit

--- Arrays6.java
+++ Arrays6.java
@@ -4,7 +4,8 @@ public class Arrays6 {
 
 	public static void main(String[] args) {
 		Arrays5 pravali = new Arrays5();
-		pravali.military();
+		System.out.println(pravali.military());
+		pravali
 	}
 
 }
